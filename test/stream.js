37efe336839173a4476cb7ac4cf55fa884f9b3ac
Rene BIGOT <rene.bigot@sncf.fr>
2017-03-28 22:38:57 +0200

    Syntax fix

--- test/stream.js
+++ test/stream.js
@@ -1,56 +1,54 @@
+let path = require('path'),
+    openal = require('../openal'),
+    spawn = require('child_process').spawn;
 
-var openal = require('../openal')
-	, spawn = require('child_process').spawn
-	, util = require('util')
+var device = new openal.Device(),
+    context = new openal.Context(device),
+    file = path.resolve(__dirname, "../sounds/Palisades.mp3"),
+    args = ["-af", "resample=44100:0:1,channels=2,format=s16le",
+            "-nogui", "-novideo", "-noconsolecontrols", "-really-quiet",
+            "-ao", "pcm:nowaveheader:file=/dev/stdout", file];
 
-
-var device = new openal.Device();
-var context = new openal.Context( device );
-
-openal.MakeContextCurrent( context );
+openal.MakeContextCurrent(context);
 openal.SetListenerOrientation(1, 0, 0);
 
+var data = new openal.WavData(path.resolve(__dirname, "../sounds/synth.wav")),
+    synth = new openal.Source(data),
+    stream = new openal.Stream(2, 16, 44100),
+    mplayer = spawn('mplayer', args);
 
-var data = new openal.WavData(__dirname+"/../sounds/synth.wav");
-var synth = new openal.Source( data );
+console.log('mplayer', args.join(' '));
 
-var stream = new openal.Stream(2, 16, 44100);
-/*
-var file = util.format("%s/../sounds/Palisades.mp3", __dirname);
-var args = ["-af", "resample=44100:0:1,channels=2,format=s16le",  
-			"-nogui", "-novideo", "-noconsolecontrols", "-really-quiet", 
-			"-ao", "pcm:nowaveheader:file=/dev/stdout", file];
-
-
-var mplayer = spawn('mplayer', args);
 mplayer.stdout.on('data', function (data) {
-	do {} while( !stream.Ready() );
-	stream.Buffer( data );
+  do {
+  } while( !stream.Ready() );
+  stream.Buffer(data);
 });
 
 mplayer.stderr.on('data', function (data) {
-	console.log('stderr: ' + data);
+  console.log('stderr: ' + data);
 });
 
 mplayer.on('close', function (code) {
-	console.log('mplayer process exited with code ' + code);
+  console.log('mplayer process exited with code ' + code);
 });
-*/
+
 
 var gain = 1;
 var x=0, y=0, z=0;
 var start = new Date();
 setInterval(function(){
-	x+=0.05;
-	gain *= 0.8;
+  console.log('interval');
+  x+=0.05;
+  gain *= 0.8;
 
-	stream.SetGain(gain);
-	stream.SetPosition(-x, y, z);
-	console.log( stream.GetPosition() );
+  stream.SetGain(gain);
+  stream.SetPosition(-x, y, z);
+  console.log( stream.GetPosition() );
 
-	openal.SetListenerPosition(x, y, z);
-	synth.Play();
-	var seconds = (new Date() - start)/1000;
-	console.log(seconds, 'secs');
+  openal.SetListenerPosition(x, y, z);
+  synth.Play();
+  var seconds = (new Date() - start)/1000;
+  console.log(seconds, 'secs');
 }, 1000);
 
